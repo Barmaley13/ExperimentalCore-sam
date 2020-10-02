1fab0992ba78de6792ba16dded66abf2d47b10b4
Kirill Belyayev <kirill.belyayev@velentium.com>
2020-10-02 16:53:44 -0600

    Exporting external delay functions

--- module/cores/arduino/port_sam/core_delay.c
+++ module/cores/arduino/port_sam/core_delay.c
@@ -16,220 +16,23 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "core_delay.h"
-#include "Arduino.h"
-#include "core_reset.h" // for tickReset()
+//#include "core_delay.h"
+//#include "Arduino.h"
+//#include "core_reset.h" // for tickReset()
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "hal_delay.h"
-#include "hpl_time_measure.h"
-
-
-void delay(uint32_t ms)
-{
-    // Calculate remainder and whole
-    uint16_t _ms = (uint16_t) (ms & 0x0000FFFF);
-    uint32_t cycles = (uint16_t) (ms >> 16);
     
-    // Remainder
-    delay_ms(_ms);
-    
-    // Whole cycles
-    while(cycles)
-    {
-        delay_ms(0xFFFF);
-        cycles--;  
-    }
-}    
-
-void delayMicroseconds(uint32_t usec)
-{
-    // Calculate remainder and whole
-    uint16_t _usec = (uint16_t) (usec & 0x0000FFFF);
-    uint32_t cycles = (uint16_t) (usec >> 16);
-    
-    // Remainder
-    delay_us(_usec);
-    
-    // Whole cycles
-    while(cycles)
-    {
-        delay_us(0xFFFF);
-        cycles--;
-    }
-}
-
-// The assumption is that those functions are used (during constant polling)
-// User should prefer delay and delayMicroseconds to the following ones
-uint32_t millis(void)
-{
-    static bool initialized = false;
-    static uint32_t ms = 0;
-    static uint32_t last_ticks = 0;
-    static uint32_t ms_ticks, max_ticks;
-
-    bool countflag;
-    uint32_t current_ticks = 0;
-    
-    if (!initialized)
-    {
-        initialized = true;
-        ms_ticks = _get_cycles_for_ms(1);
-        max_ticks = SysTick-> LOAD;
-
-        // Read to clear the flag (if needed)
-        countflag = SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk;
-            
-        // Save for the next read
-        last_ticks = SysTick->VAL;
-    }
-    else
-    {
-        // Read countflag and current_ticks so we can minimize the error
-        countflag = (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk);
-        current_ticks = SysTick->VAL;
-        
-        // Calculate milliseconds
-        if (countflag)
-            ms += (last_ticks + max_ticks - current_ticks) / ms_ticks;
-        else
-            ms += (last_ticks - current_ticks) / ms_ticks;
-        
-        // Save for the next read
-        last_ticks = current_ticks;
-    }
-
-    return ms;
-}
-
-uint32_t micros( void )
-{
-    static bool initialized = false;
-    static uint32_t us = 0;
-    static uint32_t last_ticks = 0;
-    static uint32_t us_ticks, max_ticks;
-
-    bool countflag;
-    uint32_t current_ticks = 0;
-    
-    if (!initialized)
-    {    
-        initialized = true;
-        us_ticks = _get_cycles_for_us(1);
-        max_ticks = SysTick-> LOAD;
-        
-        // Read to clear the flag (if needed)
-        countflag = SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk;
-        
-        // Save for the next read
-        last_ticks = SysTick->VAL;
-    }
-    else
-    {
-        // Read countflag and current_ticks so we can minimize the error
-        countflag = (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk);
-        current_ticks =  SysTick->VAL;
-        
-        // Calculate micro seconds
-        if (countflag)
-            us += (last_ticks + max_ticks - current_ticks) / us_ticks;
-        else
-            us += (last_ticks - current_ticks) / us_ticks;
-        
-        // Save for the next read
-        last_ticks = current_ticks;
-    }
-    
-    return us;
-}
-
-
-//void SysTick_Handler(void)
+//void delay(uint32_t ms)
 //{
-//if (sysTickHook() != 0)
+    //return delay_ms(ms);
+//}    
+//
+//void delayMicroseconds(uint32_t us)
 //{
-//return;
-//}
-//
-//SysTick_DefaultHandler();
-//}
-///** Tick Counter united by ms */
-//static volatile uint32_t _ulTickCount=0 ;
-//
-//uint32_t millis( void )
-//{
-//// todo: ensure no interrupts
-  //return _ulTickCount ;
-//}
-//
-//// Interrupt-compatible version of micros
-//// Theory: repeatedly take readings of SysTick counter, millis counter and SysTick interrupt pending flag.
-//// When it appears that millis counter and pending is stable and SysTick hasn't rolled over, use these
-//// values to calculate micros. If there is a pending SysTick, add one to the millis counter in the calculation.
-//uint32_t micros( void )
-//{
-    //uint32_t ticks, ticks2;
-    //uint32_t pend, pend2;
-    //uint32_t count, count2;
-//
-    //ticks2  = SysTick->VAL;
-    //pend2   = !!((SCB->ICSR & SCB_ICSR_PENDSTSET_Msk)||((SCB->SHCSR & SCB_SHCSR_SYSTICKACT_Msk)))  ;
-    //count2  = _ulTickCount;
-//
-    //do 
-	//{
-        //ticks=ticks2;
-        //pend=pend2;
-        //count=count2;
-        //ticks2  = SysTick->VAL;
-        //pend2   = !!((SCB->ICSR & SCB_ICSR_PENDSTSET_Msk)||((SCB->SHCSR & SCB_SHCSR_SYSTICKACT_Msk)))  ;
-        //count2  = _ulTickCount;
-    //} while ((pend != pend2) || (count != count2) || (ticks < ticks2));
-//
-    //return ((count+pend) * 1000) + (((SysTick->LOAD  - ticks)*(1048576/(VARIANT_MCK/1000000)))>>20) ;
-    //// this is an optimization to turn a runtime division into two compile-time divisions and
-    //// a runtime multiplication and shift, saving a few cycles
-//}
-
-// original function:
-// uint32_t micros( void )
-// {
-//     uint32_t ticks ;
-//     uint32_t count ;
-//
-//     SysTick->CTRL;
-//     do {
-//         ticks = SysTick->VAL;
-//         count = GetTickCount();
-//     } while (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk);
-//
-//     return count * 1000 + (SysTick->LOAD + 1 - ticks) / (SystemCoreClock/1000000) ;
-// }
-
-
-//void delay( uint32_t ms )
-//{
-  //if ( ms == 0 )
-  //{
-    //return ;
-  //}
-//
-  //uint32_t start = _ulTickCount ;
-//
-  //do
-  //{
-    //yield() ;
-  //} while ( _ulTickCount - start < ms ) ;
-//}
-//
-//void SysTick_DefaultHandler(void)
-//{
-  //// Increment tick count each ms
-  //_ulTickCount++;
-  //tickReset();
+    //return delay_us(us);
 //}
 
 #ifdef __cplusplus
--- module/cores/arduino/port_sam/core_delay.h
+++ module/cores/arduino/port_sam/core_delay.h
@@ -26,6 +26,8 @@
 extern "C" {
 #endif
 
+#include "hal_delay.h"
+
 /**
  * \brief Returns the number of milliseconds since the Arduino board began running the current program.
  *
@@ -33,7 +35,7 @@ extern "C" {
  *
  * \return Number of milliseconds since the program started (uint32_t)
  */
-extern uint32_t millis( void ) ;
+//extern uint32_t millis( void ) ;
 
 /**
  * \brief Returns the number of microseconds since the Arduino board began running the current program.
@@ -45,7 +47,7 @@ extern uint32_t millis( void ) ;
  *
  * \note There are 1,000 microseconds in a millisecond and 1,000,000 microseconds in a second.
  */
-extern uint32_t micros( void ) ;
+//extern uint32_t micros( void ) ;
 
 /**
  * \brief Pauses the program for the amount of time (in miliseconds) specified as parameter.
@@ -53,54 +55,12 @@ extern uint32_t micros( void ) ;
  *
  * \param dwMs the number of milliseconds to pause (uint32_t)
  */
-extern void delay( uint32_t dwMs ) ;
+//extern void delay( uint32_t dwMs ) ;
+//
+//void delayMicroseconds( uint32_t usec );
 
-void delayMicroseconds( uint32_t usec );
-
-///**
- //* \brief Pauses the program for the amount of time (in microseconds) specified as parameter.
- //*
- //* \param dwUs the number of microseconds to pause (uint32_t)
- //*/
-//static __inline__ void delayMicroseconds( uint32_t ) __attribute__((always_inline, unused)) ;
-//static __inline__ void delayMicroseconds( uint32_t usec )
-//{
-  //if ( usec == 0 )
-  //{
-    //return ;
-  //}
-//
-  ///*
-   //*  The following loop:
-   //*
-   //*    for (; ul; ul--) {
-   //*      __asm__ volatile("");
-   //*    }
-   //*
-   //*  produce the following assembly code:
-   //*
-   //*    loop:
-   //*      subs r3, #1        // 1 Core cycle
-   //*      bne.n loop         // 1 Core cycle + 1 if branch is taken
-   //*/
-//
-  //// VARIANT_MCK / 1000000 == cycles needed to delay 1uS
-  ////                     3 == cycles used in a loop
-  //uint32_t n = usec * (VARIANT_MCK / 1000000) / 3;
-  //__asm__ __volatile__(
-    //"1:              \n"
-    //"   sub %0, #1   \n" // substract 1 from %0 (n)
-    //"   bne 1b       \n" // if result is not 0 jump to 1
-    //: "+r" (n)           // '%0' is n variable with RW constraints
-    //:                    // no input
-    //:                    // no clobber
-  //);
-  //// https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html
-  //// https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#Volatile
-//}
-//
-///* Default Arduino systick handler */
-//extern void SysTick_DefaultHandler(void);
+#define delay               delay_ms
+#define delayMicroseconds   delay_us
 
 #ifdef __cplusplus
 }
